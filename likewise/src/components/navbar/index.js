7b4268e963770c90c1e8807c97c9b9710aca1752
Olivia Tang <tango2@uw.edu>
2020-02-18 14:39:15 -0800

    components made

---
+++ likewise/src/components/navbar/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './style.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+class index extends Component {
+    render() {
+        return (
+            <div className="navbar" style={{background: "#DCCF5FC"}}>
+            <AppBar position="static" className="bar page-container" style={{background: "#D4DFEC"}} >
+                <Toolbar>
+                    <Typography variant="h5" className="title">
+                        LIKEWISE
+                    </Typography>
+                    <Button variant="contained" id="login">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+        );
+    }
+}
+
+export default index;
