0afa5872db903b6ac6edbf6ad37b11be116dee70
Olivia Tang <tango2@uw.edu>
2020-02-18 19:58:07 -0800

    Updates

---
+++ precache-manifest.aed28743b0dbd949236fd2d3a8895e4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2146d52720d3cfd832367ab68812ff3d",
+    "url": "/likewise/index.html"
+  },
+  {
+    "revision": "4c79f56877e9b00180b0",
+    "url": "/likewise/static/css/main.bbc0f9c5.chunk.css"
+  },
+  {
+    "revision": "32260ead16a3096f1ee0",
+    "url": "/likewise/static/js/2.60e1eda5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/likewise/static/js/2.60e1eda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c79f56877e9b00180b0",
+    "url": "/likewise/static/js/main.0ea14d17.chunk.js"
+  },
+  {
+    "revision": "56aa8f4d8edcb8d3a774",
+    "url": "/likewise/static/js/runtime-main.9e9101fe.js"
+  },
+  {
+    "revision": "ff5796db8f68fc79aef451ddd70c26ba",
+    "url": "/likewise/static/media/circle.ff5796db.PNG"
+  }
+]);
