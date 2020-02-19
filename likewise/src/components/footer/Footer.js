a8ddbe6920b074e2c1db00fbddbe28252eef244d
Brian Uyeda <uyedacb@uw.edu>
2020-02-18 16:56:11 -0800

    start footer

---
+++ likewise/src/components/footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {makeStyles, Container, Button, Grid} from '@material-ui/core';
+import './style.css';
+
+export default function Footer(props) {
+    return (
+        <Box>
+            <Grid container>
+                <Grid item>
+                    <h2>
+                        Likewise
+                    </h2>
+                </Grid>
+                <Grid item>
+
+                </Grid>
+                <Grid item>
+
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
