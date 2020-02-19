3a6a1275e70fd5a7eaf51ea1af3a86f205eb50c9
Olivia Tang <tango2@uw.edu>
2020-02-18 19:38:44 -0800

    fixed button styling and attempted to do text change

--- likewise/src/components/footer/Footer.js
+++ likewise/src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {makeStyles, Container, Button, Grid, Box} from '@material-ui/core';
+import {makeStyles, Grid, Box} from '@material-ui/core';
 import './style.css';
 import circle from "../../assets/circle.PNG";
 
--- likewise/src/components/section2/Significance.js
+++ likewise/src/components/section2/Significance.js
@@ -1,69 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {makeStyles, Container, Button, Grid} from '@material-ui/core';
 import './style.css';
 
-const useStyles = makeStyles(theme => ({
-    purple: {
-        flexGrow: 1,
-        backgroundColor: "#8481E2",
-        color: "#FFF",
-        marginRight: "auto",
-        marginLeft: "auto",
-        padding: "20px 40px",
-    },
-    white: {
-        flexGrow: 1,
-        backgroundColor: "#F6F6F2",
-        color: "#000000",
-        padding: "20px 40px",
-    }
-  }));
-
-export default function Significance(props) {
-    const classes = useStyles();
-    return (
-        <div>
-            <div className={classes.purple}>
-                <h2>
-                    Have you ever felt (changing text: lost, sad, happy, etc)?
-                </h2>
-                <h3 style={{fontStyle: "italic"}}>
-                    Many of us have too.
-                </h3>
-                <p>
-                Likewise was created to be a safe space for students at UW to share the hardships 
-                they face along with the happy experiences that they have. We want you to be able to 
-                connect with others like you so that our UW community can help you overcome any difficulties 
-                you may be facing together. By making posts and responding with your advice,
-                opinions and experiences, we can also help those who are too afraid to speak up and share.
-                </p>
-            </div>
-            <div className={classes.white}>
-                <h2>
-                    How does Likewise work?
-                </h2>
-                <div style={{
-                        padding: "35px"}}>
-                    <Container style={{background: "#FFFFFF",
-                        border: "1px solid #000000",
-                        boxSizing: "border-box",
-                        borderRadius: "4px",
-                        width: "85vw",
-                        height: "60vh"}}>
-                    </Container>
+export default class Significance extends Component{
+    //let classes = useStyles();
+    render(){
+        return (
+            <div>
+                <div className="purple">
+                        <h2> Have you ever felt <span id="changeText">sad</span>?</h2>
+                    <h3 style={{fontStyle: "italic"}}>
+                        Many of us have too.
+                    </h3>
+                    <p>
+                    Likewise was created to be a safe space for students at UW to share the hardships 
+                    they face along with the happy experiences that they have. We want you to be able to 
+                    connect with others like you so that our UW community can help you overcome any difficulties 
+                    you may be facing together. By making posts and responding with your advice,
+                    opinions and experiences, we can also help those who are too afraid to speak up and share.
+                    </p>
                 </div>
+                <div className="white">
+                    <h2>
+                        How does Likewise work?
+                    </h2>
+                    <div style={{
+                            padding: "35px"}}>
+                        <Container style={{background: "#FFFFFF",
+                            border: "1px solid #000000",
+                            boxSizing: "border-box",
+                            borderRadius: "4px",
+                            width: "85vw",
+                            height: "60vh"}}>
+                        </Container>
+                    </div>
+                </div>
+                    <Grid container className="purple" style={{textAlign: "center", height: "100%"}} justify="center">
+                        <Grid item spacing={4} style={{padding: "0px 40px"}}>
+                            <h2 style={{display: "inline-flex"}}>
+                                Are you ready to start sharing?
+                            </h2>
+                        </Grid>
+                        <Grid item spacing={4} style={{padding: "10px 0px 40px"}}>
+                            <Button variant="contained" style={{display: "inline-flex",
+                                color: "#FFFFFF", backgroundColor: "#000000", padding:"1rem"}}>Join the community</Button>
+                        </Grid>
+                    </Grid>
             </div>
-                <Grid container className={classes.purple} style={{textAlign: "center", height: "100%"}} justify="center">
-                    <Grid item spacing={4} style={{padding: "0px 40px"}}>
-                        <h2 style={{display: "inline-flex"}}>
-                            Are you ready to start sharing?
-                        </h2>
-                    </Grid>
-                    <Grid item spacing={4} style={{padding: "10px 0px 40px"}}>
-                        <Button variant="contained" style={{display: "inline-flex",
-                            color: "#FFFFFF", backgroundColor: "#000000"}}>Join the community</Button>
-                    </Grid>
-                </Grid>
-        </div>
-    );
+        );
+    }
+   
 }
